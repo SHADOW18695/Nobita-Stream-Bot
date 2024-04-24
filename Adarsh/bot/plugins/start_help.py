cbf86f945bddcb67e82ea07391e9a6cbb8154deb
SHADOW18695 <99009597+SHADOW18695@users.noreply.github.com>
2024-04-24 23:38:50 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -3,7 +3,7 @@ from Adarsh.bot import StreamBot
 from Adarsh.vars import Var
 import logging
 logger = logging.getLogger(__name__)
-#from Adarsh.bot.plugins.stream import MY_PASS
+from Adarsh.bot.plugins.stream import MY_PASS
 from Adarsh.utils.human_readable import humanbytes
 from Adarsh.utils.database import Database
 from pyrogram import filters
@@ -13,27 +13,27 @@ from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
 
-# if MY_PASS:
-#             buttonz=ReplyKeyboardMarkup(
-#             [
-#                 ["start⚡️","help📚"],
-#                 ["login🔑","DC"],
-#                 ["follow❤️","ping📡"],
-#                 ["status📊","owner😎"]
+if MY_PASS:
+            buttonz=ReplyKeyboardMarkup(
+            [
+                ["start⚡️","help📚"],
+                ["login🔑","DC"],
+                ["follow❤️","ping📡"],
+                ["status📊","owner😎"]
                         
-#             ],
-#             resize_keyboard=True
-#         )
-# else:
-#             buttonz=ReplyKeyboardMarkup(
-#             [
-#                 ["help📚","DC"],
-#                 ["follow❤️","ping📡"],
-#                 ["status📊","owner😎"]
+            ],
+            resize_keyboard=True
+        )
+else:
+            buttonz=ReplyKeyboardMarkup(
+            [
+                ["help📚","DC"],
+                ["follow❤️","ping📡"],
+                ["status📊","owner😎"]
                         
-#             ],
-#             resize_keyboard=True
-#         )
+            ],
+            resize_keyboard=True
+        )
 
             
             
